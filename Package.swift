25accc0b598507b453aff971bab1a7e3ae700485
dmitry <Buza.dima93@gmail.com>
2020-02-18 14:46:18 +0600

    Removed to Sources folder

--- Package.swift
+++ Package.swift
@@ -29,17 +29,21 @@
 
 import PackageDescription
 
-let package = Package(name: "AlamofireObjectMapper",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v10),
-                                  .tvOS(.v10),
-                                  .watchOS(.v3)],
-                      products: [.library(name: "AlamofireObjectMapper",
-                                          targets: ["AlamofireObjectMapper"])],
-                      targets: [.target(name: "AlamofireObjectMapper",
-                                        path: "AlamofireObjectMapper")],
-                      dependencies: [
-                        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0-rc.3"),
-                          .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "3.5.1")
-                      ],
-                      swiftLanguageVersions: [.v5])
+//let package = Package(
+
+let package = Package (
+    name: "AlamofireObjectMapper",
+    products: [
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0-rc.3"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "3.5.1")],
+    targets: [
+        .target(
+            name: "AlamofireObjectMapper",
+            dependencies: [])
+    ]
+)
